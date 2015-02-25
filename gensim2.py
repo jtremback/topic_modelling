a58897a9d59006739fa161d7f077f672e2991474
Jehan <jehan.tremback@gmail.com>
2015-02-25 15:10:21 -0800

    more progress

--- gensim2.py
+++ gensim2.py
@@ -1,60 +1,40 @@
-# from gensim import corpora, models, similarities
-
-# documents = ["Human machine interface for lab abc computer applications",
-#              "A survey of user opinion of computer system response time",
-#              "The EPS user interface management system",
-#              "System and human system engineering testing of EPS",
-#              "Relation of user perceived response time to error measurement",
-#              "The generation of random binary unordered trees",
-#              "The intersection graph of paths in trees",
-#              "Graph minors IV Widths of trees and well quasi ordering",
-#              "Graph minors A survey"]
-
-# # remove common words and tokenize
-# stoplist = set('for a of the and to in'.split())
-# texts = [[word for word in document.lower().split() if word not in stoplist]
-#          for document in documents]
-
-# # remove words that appear only once
-# all_tokens = sum(texts, [])
-# tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-# texts = [[word for word in text if word not in tokens_once]
-#          for text in texts]
-
-# # print(texts)
-
-# dictionary = corpora.Dictionary(texts)
-# dictionary.save('/tmp/deerwester.dict') # store the dictionary, for future reference
-# # print(dictionary.token2id)
-
-# # new_doc = "Human computer interaction was a stinky"
-# # new_vec = dictionary.doc2bow(new_doc.lower().split())
-# # print(new_vec)
-
-# corpus = [dictionary.doc2bow(text) for text in texts]
-# # print(corpus)
+from gensim import corpora, models, similarities, utils
+import json
+import re
 
 
 # collect statistics about all tokens
-dictionary = corpora.Dictionary(line.lower().split() for line in open('mycorpus.txt'))
+dictionary = corpora.Dictionary(
+  re.sub(r'[^\w|\ ]|_|\d', '', line).lower().split()
+  for line in open('few_documents')
+)
 
-# remove stop words and words that appear only once
-stop_ids = [dictionary.token2id[stopword] for stopword in stoplist
-            if stopword in dictionary.token2id]
+# filter out extremes
+dictionary.filter_extremes(2, .01)
 
-once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-dictionary.filter_tokens(stop_ids + once_ids) # remove stop words and words that appear only once
-dictionary.compactify() # remove gaps in id sequence after words that were removed
+# stoplist = set('for a of the and to in'.split())
+
+# # remove stop words and words that appear only once
+# stop_ids = [dictionary.token2id[stopword] for stopword in stoplist
+#             if stopword in dictionary.token2id]
+
+# once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+# dictionary.filter_tokens(stop_ids + once_ids) # remove stop words and words that appear only once
+# dictionary.compactify() # remove gaps in id sequence after words that were removed
+dictionary.save('dict')
+dictionary.save_as_text('dict.txt')
 print(dictionary)
 
 
-class MyCorpus(object):
-  def __iter__(self):
-    for line in open('mycorpus.txt'):
-      # assume there's one document per line, tokens separated by whitespace
-      yield dictionary.doc2bow(line.lower().split())
+# class MyCorpus(object):
+#   def __iter__(self):
+#     for line in open('documents'):
+#       # assume there's one document per line, tokens separated by whitespace
+#       yield dictionary.doc2bow(line.lower().split())
 
-corpus_memory_friendly = MyCorpus()
+# corpus_memory_friendly = MyCorpus()
 
-for vector in corpus_memory_friendly: # load one vector into memory at a time
-  print(vector)
+# # for vector in corpus_memory_friendly: # load one vector into memory at a time
+# #   print(vector)
+
+# corpora.MmCorpus.serialize('mmcorpus', corpus_memory_friendly)
--- index.py
+++ index.py
@@ -1,9 +1,59 @@
+from __future__ import print_function
 import json
+import leveldb
 
-f = open('db')
+db = leveldb.LevelDB('./db')
+subset = open('subset')
+documents = open('documents', 'w')
 
-for line in iter(f):
-    line = json.loads(line)
-    terms = line['val']
+# count = 0
 
-f.close()
+# for line in iter(log):
+#   json_line = json.loads(line)
+
+#   if 'terms' in json_line['val']:
+#     count += 1
+#     if count % 1000 == 0:
+#       print(count)
+
+#     db.Put(str(json_line['key']), json.dumps(json_line['val']))
+
+
+# count = 0
+
+# for doc in db.RangeIter():
+#   count += 1
+#   if count % 1000 == 0:
+#     print(count)
+#   if count == 10000:
+#     break
+#   subset.write(doc[1] + '\n')
+
+
+# count = 0
+
+# for doc in db.RangeIter():
+#   json_val = json.loads(doc[1])
+
+#   count += 1
+#   if count % 1000 == 0:
+#     print(count)
+#   if count == 10000:
+#     break
+
+#   if json_val['lang_terms'] == 'en':
+#     for term in json_val['terms']:
+
+#       documents.write(term['definition'].encode('utf8') + ' ' + term['term'].encode('utf8') + '\n')
+
+# count = 0
+
+# for line in iter(subset):
+#   count += 1
+#   print(count)
+
+#   json_line = json.loads(line)
+
+#   for term in json_line['terms']:
+#     documents.write(term['definition'].encode('utf8') + ' ' + term['term'].encode('utf8') + '\n
+
